da147e511d2d106f9f4586994666ec7b15de1b50
Daniel Hernandez <danielhp95@gmail.com>
2019-01-30 12:27:48 +0000

    Removed sterile code and updated agent_hook import

--- rl_algorithms/tabular_q_learning.py
+++ rl_algorithms/tabular_q_learning.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-import copy
+
 
 class TabularQLearningAlgorithm():
 
@@ -14,28 +14,19 @@ class TabularQLearningAlgorithm():
         self.training = training
         self.name = 'TabularQLearning'
         pass
-    
+
     def update_q_table(self, s, a, r, succ_s):
         self.Q_table[s, a] += self.learning_rate * (r + max(self.Q_table[succ_s, :]) - self.Q_table[s, a])
 
-    
     def find_optimal_moves(self, Q_table, state):
         optimal_moves = np.argwhere(Q_table[state, :] == np.amax(Q_table[state, :]))
         return optimal_moves.flatten().tolist()
 
-    def clone(self, training=False):
-        cloned = TabularQLearning(self.Q_table.shape[0], self.Q_table.shape[1], self.hashing_function,
-                                  learning_rate=self.learning_rate, training=training)
-        cloned.Q_table = copy.deepcopy(self.Q_table)
-        return cloned
 
-    
 class TabularQLearningAgent(TabularQLearningAlgorithm):
     def __init__(self, state_space_size, action_space_size, hashing_function, learning_rate=0.5, training=True):
         """
-        TODO: 
-        Document
-        Remove clone4 + queue2
+        TODO: Document
         """
         self.Q_table = np.zeros((state_space_size, action_space_size), dtype=np.float64)
         self.learning_rate = learning_rate
@@ -57,13 +48,10 @@ class TabularQLearningAgent(TabularQLearningAlgorithm):
         pass
 
     def clone(self, training=None, path=None):
-        from .interface import AgentHook
-        cloned = AgentHook(self,training=training, path=path)
+        from .agent_hook import AgentHook
+        cloned = AgentHook(self, training=training, path=path)
         return cloned
 
-def build_TabularQ_Agent(state_space_size,
-                            action_space_size,
-                            hash_state
-                            ) :
-    return TabularQLearningAgent(state_space_size, action_space_size, hash_state)
 
+def build_TabularQ_Agent(state_space_size, action_space_size, hash_state):
+    return TabularQLearningAgent(state_space_size, action_space_size, hash_state)
