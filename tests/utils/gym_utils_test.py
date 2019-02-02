768a8fa7da9a94c4a013f33aed6006e02b07c8c3
Daniel Hernandez <danielhp95@gmail.com>
2019-02-02 11:41:44 +0000

    Updated tests and documentation

--- tests/utils/gym_utils_test.py
+++ tests/utils/gym_utils_test.py
@@ -5,6 +5,7 @@ import pytest
 
 import util
 
+
 @pytest.fixture
 def env():
     import gym
@@ -18,18 +19,18 @@ def single_agent_env():
     return gym.make('CartPole-v0')
 
 
-def test_obtain_observation_dimensions(env):
+def test_get_observation_dimensions(env):
     expected_space_dimensions = 120
-    space_dimensions = util.gym_utils.obtain_observation_dimensions(env)
+    space_dimensions = util.gym_utils.get_observation_dimensions(env)
     assert space_dimensions == expected_space_dimensions
 
 
-def test_obtain_action_dimensions(env):
+def test_get_action_dimensions(env):
     expected_space_dimensions = 120
-    space_dimensions = util.gym_utils.obtain_observation_dimensions(env)
+    space_dimensions = util.gym_utils.get_observation_dimensions(env)
     assert space_dimensions == expected_space_dimensions
 
 
 def test_fail_single_agent_environment(single_agent_env):
     with pytest.raises(ValueError) as _:
-        util.gym_utils.obtain_observation_dimensions(single_agent_env)
+        util.gym_utils.get_observation_dimensions(single_agent_env)
