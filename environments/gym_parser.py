aec8760a2a3fbba01d1704f9157a1d2008b7ceb2
Daniel Hernandez <danielhp95@gmail.com>
2019-02-15 22:25:17 +0000

    Added assert in gym_parser test and simplified slightly naive-self-play-deep-q-learning test

--- environments/gym_parser.py
+++ environments/gym_parser.py
@@ -6,7 +6,7 @@ from gym.spaces import Box, Discrete, Tuple
 
 from collections import namedtuple
 
-Task = namedtuple('Task', 'name observation_dim observation_type action_dim action_type')
+Task = namedtuple('Task', 'name observation_dim observation_type action_dim action_type hash_function')
 
 
 def get_observation_dimensions_and_type(env):
@@ -38,4 +38,5 @@ def parse_gym_environment(env):
     name = env.spec.id
     action_dims, action_type = get_action_dimensions_and_type(env)
     observation_dims, observation_type = get_observation_dimensions_and_type(env)
-    return Task(name, observation_dims, observation_type, action_dims, action_type)
+    hash_function = env.hash_state if hasattr(env, 'hash_state') else None
+    return Task(name, observation_dims, observation_type, action_dims, action_type, hash_function)
--- tests/integration/naive-self-play-training-deep-q-learning.py
+++ tests/integration/naive-self-play-training-deep-q-learning.py
@@ -11,36 +11,37 @@ import numpy as np
 import gym
 import gym_rock_paper_scissors
 from gym_rock_paper_scissors.fixed_agents import rockAgent, paperAgent, scissorsAgent
-import environments 
-	
+import environments
+
+
 def test_selfplay_DQNvsFixedAgent():
+	torch.multiprocessing.set_start_method('spawn')
 	target_episodes = 1000
 	env = gym.make('RockPaperScissors-v0')
 	env.__init__(stacked_observations=2, max_repetitions=1)
 	task = environments.parse_gym_environment(env)
-	
-	training_policy = build_DQN_Agent(state_space_size=task.observation_dim, 
-										action_space_size=task.action_dim, 
-										double=True, 
-										dueling=True,
-										num_worker=1,
-										MIN_MEMORY=1e1,
-										epsstart=0.9,
-										epsend=0.01,
-										epsdecay=2e2,
-										use_cuda=False)
+
+	training_policy = build_DQN_Agent(state_space_size=task.observation_dim,
+                                          action_space_size=task.action_dim,
+                                          double=True,
+                                          dueling=True,
+                                          num_worker=1,
+                                          MIN_MEMORY=1e1,
+                                          epsstart=0.9,
+                                          epsend=0.01,
+                                          epsdecay=2e2,
+                                          use_cuda=False)
 
 	fixedAgent = rockAgent
 
 	policy = simultaneous_action_rl_loop.self_play_training(env=env,
-															menagerie=[fixedAgent],
-															training_agent=AgentHook(training_policy),
-	                                                        self_play_scheme=NaiveSelfPlay, 
-	                                                        target_episodes=target_episodes, 
-	                                                        results_path='./test',
+                                                                menagerie=[fixedAgent],
+                                                                training_agent=AgentHook(training_policy),
+	                                                        self_play_scheme=NaiveSelfPlay,
+	                                                        target_episodes=target_episodes,
+	                                                        results_path='/tmp',
 	                                                        opci=1)
 
-	
+
 if __name__ == '__main__':
-	torch.multiprocessing.set_start_method('spawn') 
 	test_selfplay_DQNvsFixedAgent()
