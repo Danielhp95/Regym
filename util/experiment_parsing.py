8d834f556b7ee50bff6b90d2b4878dc4c728e3ea
Daniel Hernandez <danielhp95@gmail.com>
2019-02-15 13:29:16 +0000

    Minor formating

--- util/experiment_parsing.py
+++ util/experiment_parsing.py
@@ -5,11 +5,12 @@ from training_schemes import EmptySelfPlay, NaiveSelfPlay, HalfHistorySelfPlay, 
 
 from rl_algorithms import build_DQN_Agent
 from rl_algorithms import build_TabularQ_Agent
-#from rl_algorithms import build_PPO_Agent
+# from rl_algorithms import build_PPO_Agent
 from rl_algorithms import rockAgent, paperAgent, scissorsAgent
 from rl_algorithms import AgentHook
 
 from .gym_parser import parse_gym_environment
+
 
 def initialize_training_schemes(training_schemes_cli):
     def parse_training_scheme(training_scheme):
@@ -22,14 +23,15 @@ def initialize_training_schemes(training_schemes_cli):
 
 def initialize_algorithms(environment, algorithms_cli, base_path):
     task = parse_gym_environment(environment)
+
     def parse_algorithm(algorithm, task):
         if algorithm.lower() == 'tabularqlearning':
             return build_TabularQ_Agent(task.observation_dim, task.action_dim)
         if algorithm.lower() == 'deepqlearning':
             # TODO Should use_cuda be pased as parameter?
             return build_DQN_Agent(state_space_size=task.observation_dim, action_space_size=task.action_dim, double=False, dueling=False, use_cuda=False)
-        #if algorithm.lower() == 'ppo':
-        #    return build_PPO_Agent(env)
+        # if algorithm.lower() == 'ppo':
+        #     return build_PPO_Agent(env)
         else: raise ValueError('Unknown algorithm {}. Try defining it inside this script.'.format(algorithm))
 
     return [parse_algorithm(algorithm, task) for algorithm in algorithms_cli], [os.path.join(base_path, algorithm.lower())+'.pt' for algorithm in algorithms_cli]
