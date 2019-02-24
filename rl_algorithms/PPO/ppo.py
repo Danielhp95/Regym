2874f20f659c79bc9db64451b0c48518f302fa43
Daniel Hernandez <danielhp95@gmail.com>
2019-02-24 15:41:28 +0000

    added model and use_cuda from kwargs to constructor

--- rl_algorithms/PPO/ppo.py
+++ rl_algorithms/PPO/ppo.py
@@ -10,6 +10,7 @@ class PPOAlgorithm():
         episode_interval_per_training:
         discount:
         use_gae:
+        use_cuda:
         gae_tau:
         entropy_weight:
         gradient_clip:
@@ -24,8 +25,12 @@ class PPOAlgorithm():
         "PER_alpha": float, alpha value for the Prioritized Experience Replay buffer.
         "use_cuda": boolean to specify whether to use CUDA.
         '''
-        self.config = deepcopy(kwargs)
-        # self.optimizer = optim.Adam(self.network.parameters(), lr=kwargs['learning_rate'], eps=kwargs['adam_eps'])
+        self.kwargs = deepcopy(kwargs)
+        self.model = self.kwargs['model']
+        if self.kwargs['use_cuda']:
+            self.model = self.model.cuda()
+
+        # self.optimizer = optim.Adam(self.model.parameters(), lr=kwargs['learning_rate'], eps=kwargs['adam_eps'])
 
     def optimize_model(self):
         raise NotImplementedError('Not yet implemented')
