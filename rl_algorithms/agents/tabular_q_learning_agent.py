3b5c732bbf22fd0bc57b44aad4ac83c442cb87eb
Daniel Hernandez <danielhp95@gmail.com>
2019-02-24 15:39:02 +0000

    updated tql

--- rl_algorithms/agents/tabular_q_learning_agent.py
+++ rl_algorithms/agents/tabular_q_learning_agent.py
@@ -2,16 +2,17 @@ import random
 from ..TQL import TabularQLearningAlgorithm
 
 
-class TabularQLearningAgent(TabularQLearningAlgorithm):
-    def __init__(self, state_space_size, action_space_size, hashing_function, learning_rate=0.5, training=True):
-        super(TabularQLearningAgent, self).__init__(state_space_size, action_space_size, hashing_function, learning_rate, training)
+class TabularQLearningAgent():
+    def __init__(self, algorithm):
+        self.name = 'TabularQLearning'
+        self.algorithm = algorithm
 
     def handle_experience(self, s, a, r, succ_s, done=False):
-        if self.training:
-            self.update_q_table(self.hashing_function(s), a, r, self.hashing_function(succ_s))
+        if self.algorithm.training:
+            self.algorithm.update_q_table(self.hashing_function(s), a, r, self.hashing_function(succ_s))
 
     def take_action(self, state):
-        optimal_moves = self.find_optimal_moves(self.Q_table, self.hashing_function(state))
+        optimal_moves = self.algorithm.find_optimal_moves(state)
         return random.choice(optimal_moves)
 
     def clone(self, training=None, path=None):
@@ -23,5 +24,6 @@ class TabularQLearningAgent(TabularQLearningAlgorithm):
 def build_TabularQ_Agent(task, config):
     state_space_size, action_space_size = task.state_space_size, task.action_dim
     hash_state = task.hash_function
-    return TabularQLearningAgent(state_space_size, action_space_size, hash_state,
-                                 learning_rate=config['learning_rate'])
+    vanilla_q_learning = TabularQLearningAlgorithm(state_space_size, action_space_size, hash_state,
+                                                   learning_rate=config['learning_rate'])
+    return TabularQLearningAgent(algorithm=vanilla_q_learning)
