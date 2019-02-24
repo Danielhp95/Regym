0e76d748818057dd8a74a928c185bfe738c0d64c
Daniel Hernandez <danielhp95@gmail.com>
2019-02-24 15:52:01 +0000

    Refactored some of the ppo agent creation into build_PPO_Agent function

--- rl_algorithms/agents/ppo_agent.py
+++ rl_algorithms/agents/ppo_agent.py
@@ -4,21 +4,28 @@
 # declaration at the top                                              #
 #######################################################################
 
-from ..network import *
+from ..networks import CategoricalActorCriticNet, GaussianActorCriticNet
+from ..networks import FCBody
+from ..networks import PreprocessFunction
 from ..PPO import PPOAlgorithm
-from ..component import *
+
+import torch.nn.functional as F
 
 
 class PPOAgent():
 
     def __init__(self, algorithm):
         self.algorithm = algorithm
+        self.training = False
+        self.state_preprocessing = self.algorithm.kwargs['state_preprocess']
 
     def handle_experience(self, s, a, r, succ_s, done=False):
         raise NotImplementedError('Not yet implemented')
 
     def take_action(self, state):
-        raise NotImplementedError('Not yet implemented')
+        state = self.state_preprocessing(state)
+        prediction = self.algorithm.model(state)
+        return prediction['a'].cpu().detach().numpy()
 
     def clone(self, training=None, path=None):
         from ..agent_hook import AgentHook
@@ -32,8 +39,20 @@ def build_PPO_Agent(task, config):
     :param config: Dict containing configuration for ppo agent
     :returns: PPOAgent adapted to be trained on :param: task under :param: config
     '''
-    kwargs = dict()
-    kwargs['model'] = CategoricalActorCriticNet(task.observation_dim, task.action_dim,
-                                                NatureConvBody())
+    kwargs = config.copy()
+    kwargs['state_preprocess'] = PreprocessFunction(task.observation_dim, kwargs['use_cuda'])
+
+    if task.action_type is 'Discrete' and task.observation_type is 'Discrete':
+        kwargs['model'] = CategoricalActorCriticNet(task.observation_dim, task.action_dim,
+                                                    phi_body=FCBody(task.observation_dim, hidden_units=(64, 64), gate=F.leaky_relu),
+                                                    actor_body=None,
+                                                    critic_body=None)
+    if task.action_type is 'Continuous' and task.observation_type is 'Continuous':
+        kwargs['model'] = GaussianActorCriticNet(task.observation_dim, task.action_dim,
+                                                 phi_body=FCBody(task.observation_dim, hidden_units=(64, 64), gate=F.leaky_relu),
+                                                 actor_body=None,
+                                                 critic_body=None)
+    assert kwargs['model'] is not None
+
     ppo_algorithm = PPOAlgorithm(kwargs)
     return PPOAgent(algorithm=ppo_algorithm)
