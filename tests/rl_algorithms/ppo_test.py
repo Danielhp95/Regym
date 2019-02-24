ed7f173bbefb87a09fc1b249c4c0dd40a90b54f4
Daniel Hernandez <danielhp95@gmail.com>
2019-02-24 15:38:15 +0000

    updated tests

--- tests/rl_algorithms/ppo_test.py
+++ tests/rl_algorithms/ppo_test.py
@@ -1,17 +1,28 @@
 import os
 import sys
 sys.path.append(os.path.abspath('../../'))
+import pytest
 
+import numpy as np
+import torch
+
+from rl_algorithms.agents import build_PPO_Agent
 from rl_algorithms.PPO import PPOAlgorithm
 from environments.gym_parser import parse_gym_environment
-import pytest
+
+from unittest.mock import Mock
 
 
 @pytest.fixture
-def RPSTask():
+def RPSenv():
     import gym
     import gym_rock_paper_scissors
-    return parse_gym_environment(gym.make('RockPaperScissors-v0'))
+    return gym.make('RockPaperScissors-v0')
+
+
+@pytest.fixture
+def RPSTask(RPSenv):
+    return parse_gym_environment(RPSenv)
 
 
 @pytest.fixture
@@ -19,6 +30,7 @@ def ppo_config_dict():
     config = dict()
     config['discount'] = 0.99
     config['use_gae'] = True
+    config['use_cuda'] = False
     config['gae_tau'] = 0.95
     config['entropy_weight'] = 0.01
     config['gradient_clip'] = 5
@@ -33,9 +45,30 @@ def ppo_config_dict():
 
 @pytest.fixture
 def ppo_kwargs(ppo_config_dict):
-    return ppo_config_dict
+    kwargs = ppo_config_dict.copy()
+    mockNN = Mock(torch.nn.Module)
+    mockNN.parameters.return_value = []
+    kwargs['model'] = mockNN
+    return kwargs
 
 
-def test_creation_ppo_algorithm_from_config(RPSTask, ppo_kwargs):
-    algorithm = PPOAlgorithm(ppo_kwargs)
-    assert all([i in ppo_kwargs.items() for i in algorithm.config.items()])
+# def test_creation_ppo_algorithm_from_kwargs(RPSTask, ppo_kwargs):
+#     algorithm = PPOAlgorithm(ppo_kwargs)
+#     print(ppo_kwargs.items())
+#     for kwarg in algorithm.kwargs.items():
+#         print(kwarg)
+#         assert kwarg in ppo_kwargs.items()
+
+
+def test_ppo_can_take_actions(RPSenv, RPSTask, ppo_config_dict):
+    agent = build_PPO_Agent(RPSTask, ppo_config_dict)
+    number_of_actions = 30
+    for i in range(number_of_actions):
+        random_observation = RPSenv.observation_space.sample()[0]
+        a = agent.take_action(random_observation)
+        print(a)
+        assert RPSenv.action_space.contains((a, a))
+
+
+# def test_creation_ppo_agent_from_config(RPSTask, ppo_config_dict):
+#     assert False
--- tests/rl_algorithms/tabular_q_learning_test.py
+++ tests/rl_algorithms/tabular_q_learning_test.py
@@ -3,25 +3,56 @@ import sys
 sys.path.append(os.path.abspath('../../'))
 
 from rl_algorithms.TQL import TabularQLearningAlgorithm
-import numpy as np
+from rl_algorithms.agents import build_TabularQ_Agent
+from environments.gym_parser import parse_gym_environment
 import pytest
 
 
 @pytest.fixture
-def env():
-    class Env():
-        state_space_size  = 5
-        action_space_size = 5
-        hashing_function = lambda x: x
-    return Env()
+def RPSenv():
+    import gym
+    import gym_rock_paper_scissors
+    return gym.make('RockPaperScissors-v0')
 
 
-def test_algorithm_instantiation(env):
-    learning_rate = 0.1
+@pytest.fixture
+def RPSTask(RPSenv):
+    return parse_gym_environment(RPSenv)
+
+
+@pytest.fixture
+def tabular_q_learning_config_dict():
+    config = dict()
+    config['learning_rate'] = 0.5
+    return config
+
+
+def test_creation_tabular_q_learning_algorithm_from_task_and_config(RPSTask, tabular_q_learning_config_dict):
     training = False
-    algorithm = TabularQLearningAlgorithm(env.state_space_size, env.action_space_size,
-                                 env.hashing_function, learning_rate, training)
-    assert algorithm.Q_table.shape == (env.state_space_size, env.action_space_size)
-    assert algorithm.learning_rate == learning_rate
-    assert algorithm.hashing_function == env.hashing_function
-    assert algorithm.training == training
+    agent = build_TabularQ_Agent(RPSTask, tabular_q_learning_config_dict)
+    assert agent.algorithm.Q_table.shape == (RPSTask.state_space_size, RPSTask.action_space_size)
+    assert agent.algorithm.learning_rate == tabular_q_learning_config_dict['learning_rate']
+    assert agent.algorithm.hashing_function == RPSTask.hash_function
+    assert agent.algorithm.training == training
+
+
+def test_tabular_q_learning_can_take_actions(RPSenv, RPSTask, tabular_q_learning_config_dict):
+    agent = build_TabularQ_Agent(RPSTask, tabular_q_learning_config_dict)
+    number_of_actions = 30
+    for i in range(number_of_actions):
+        # asumming that first observation corresponds to observation space of this agent
+        random_observation = RPSenv.observation_space.sample()[0]
+        a = agent.take_action(random_observation)
+        assert RPSenv.action_space.contains([a, a])
+
+
+def learns_to_beat_rock_in_RPS(RPSenv, RPSTask, tabular_q_learning_config_dict):
+    '''
+    Test used to make sure that agent is 'learning' by learning a best response
+    against an agent that only plays rock in rock paper scissors.
+    i.e from random, learns to play only (or mostly) paper
+    '''
+    agent = build_TabularQ_Agent(RPSTask, tabular_q_learning_config_dict)
+
+
+# def can_be_used_with_agent_hook_test()
