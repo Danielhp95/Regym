6291f8f07e5c2c29ff832d1ef25f2a934d260afd
Denamganai Kevin <denamganai.kevin@gmail.com>
2019-02-26 20:23:38 +0000

    deprecation of the old agentHook and test for the new one in tests....

--- rl_algorithms/agent_hook.py
+++ rl_algorithms/agent_hook.py
@@ -9,7 +9,7 @@ import copy
 AgentType = Enum("AgentType", "DQN TQL DDPG PPO MixedStrategyAgent")
 
 
-class AgentHook():
+class AgentHook_deprec():
     def __init__(self, agent, training=None, path=None):
         """
         AgentHook is to be used as a saver and loader object for agents prior to or following their transportation from
@@ -26,6 +26,7 @@ class AgentHook():
         if isinstance(agent, DeepQNetworkAgent): self.init_dqn_agent(agent, training, path)
         elif isinstance(agent, TabularQLearningAgent): self.init_tql_agent(agent, training, path)
         elif isinstance(agent, DDPGAgent): self.init_ddpg_agent(agent, training, path)
+        elif isinstance(agent, PPOAgent): self.init_ppo_agent(agent, training, path)
         elif isinstance(agent, MixedStrategyAgent): self.init_mixedstrategy_agent(agent, training, path)
         else: raise ValueError('Unknown AgentType {}, valid types are: {}'.format(type(agent), [t for t in AgentType]))
 
@@ -33,6 +34,7 @@ class AgentHook():
         if self.type == AgentType.TQL: return copy.deepcopy(self.agent)
         if self.type == AgentType.DQN: return self.call_dqn(training, use_cuda)
         if self.type == AgentType.DDPG: return self.call_ddpg(training, use_cuda)
+        if self.type == AgentType.PPO: return self.call_ppo(training, use_cuda)
         if self.type == AgentType.MixedStrategyAgent: return self.agent
         else: raise ValueError('Unknown AgentType {}, valid types are: {}'.format(self.type, [t for t in AgentType]))
 
@@ -68,12 +70,63 @@ class AgentHook():
         else:
             self.agent = copy.deepcopy(agent)
 
+    def init_ppo_agent(self, agent, training, path):
+        self.path = path
+        self.type = AgentType.PPO
+        self.kwargs = dict()
+        for name in agent.kwargs:
+            if 'model' not in name: self.kwargs[name] = agent.kwargs[name]
+        # Saving CPU state_dict and Replay Buffer:
+        if self.path is not None:
+            model = agent.getModel()
+            torch.save(model.cpu().state_dict(), self.path)
+        else:
+            self.agent = copy.deepcopy(agent)
+
     def init_mixedstrategy_agent(self, agent, training, path):
         self.type = AgentType.MixedStrategyAgent
         self.agent = agent
 
     def call_ppo(self, training, use_cuda):
-        raise NotImplementedError('PPO agent hook has not been implemented')
+        # TODO Test ppo as a whole
+        if self.path is None: return self.agent
+
+        if use_cuda is not None:
+            self.kwargs['use_cuda'] = use_cuda
+        # Init Model:
+        kwargs['state_preprocess'] = PreprocessFunction(task.observation_dim, kwargs['use_cuda'])
+
+        raise NotImplementedError('')
+
+        #TODO : build the model with the correct argument from the ?task information?
+        if self.kwargs['task_action_type'] is 'Discrete' and self.kwargs['task_observation_type'] is 'Discrete':
+            model = CategoricalActorCriticNet(task.observation_dim, task.action_dim,
+                                                        phi_body=FCBody(task.observation_dim, hidden_units=(64, 64), gate=F.leaky_relu),
+                                                        actor_body=None,
+                                                        critic_body=None)
+        if task.action_type is 'Continuous' and task.observation_type is 'Continuous':
+            kwargs['model'] = GaussianActorCriticNet(task.observation_dim, task.action_dim,
+                                                     phi_body=FCBody(task.observation_dim, hidden_units=(64, 64), gate=F.leaky_relu),
+                                                     actor_body=None,
+                                                     critic_body=None)
+
+        #model_actor = ActorNN(state_dim=self.kwargs['state_dim'], action_dim=self.kwargs['action_dim'], action_scaler=self.kwargs['action_scaler'], actfn=self.kwargs['actfn'], use_cuda=False)
+        #model_critic = CriticNN(state_dim=self.kwargs['state_dim'], action_dim=self.kwargs['action_dim'], HER=self.kwargs['HER']['use_her'], actfn=self.kwargs['actfn'], use_cuda=False)
+        
+        # Loading CPU state_dict:
+        model.load_state_dict(torch.load(self.path))
+        if self.kwargs['use_cuda']:
+            model = model.cuda()
+        
+        # Init Algorithm
+        kwargs = copy.deepcopy(self.kwargs)
+        kwargs['model'] = model
+        algorithm = PPOAlgorithm(kwargs)
+        
+        # Init Agent
+        agent = PPOAgent(algorithm=algorithm)
+        if training is not None: agent.training = training
+        return agent
 
     def call_dqn(self, training, use_cuda):
         if self.path is None: return self.agent
@@ -130,6 +183,45 @@ class AgentHook():
         if agent.training: agent.algorithm.replayBuffer.load(self.path)
         return agent
 
+    def clone(self, training=None, path=None):
+        cloned = copy.deepcopy(self)
+        cloned.training = training
+        return cloned
+
+
+
+
+class AgentHook():
+    def __init__(self, agent, training=None, path=None):
+        """
+        AgentHook is to be used as a saver and loader object for agents prior to or following their transportation from
+        a training scheme to a benchmarking scheme etc...
+
+        :param agent: any agent to hang for transportation between processes.
+        :param path: path where to save the current agent.
+        """
+
+        self.name = agent.name
+        self.training = training
+        self.type = None
+
+        self.path = path
+        torch.save(agent, self.path)
+
+    def __call__(self, training=None, use_cuda=None):
+        agent = torch.load(self.path)
+        if training is not None :
+            self.training = training
+        agent.training = self.training
+        if use_cuda is not None :
+            agent.algorithm.kwargs['use_cuda'] = use_cuda
+            if agent.algorithm.kwargs['use_cuda']:
+                agent.algorithm.model = agent.algorithm.model.cuda()
+            else:
+                agent.algorithm.model = agent.algorithm.model.cpu()
+
+        return agent 
+    
     def clone(self, training=None, path=None):
         cloned = copy.deepcopy(self)
         cloned.training = training
--- tests/rl_algorithms/agents_test.py
+++ tests/rl_algorithms/agents_test.py
@@ -7,43 +7,81 @@ from rl_algorithms.agents import build_TabularQ_Agent, TabularQLearningAgent
 from rl_algorithms.agents import build_DQN_Agent, DeepQNetworkAgent 
 from rl_algorithms.agents import build_DDPG_Agent, DDPGAgent 
 from rl_algorithms.agent_hook import AgentHook
+from environments.gym_parser import parse_gym_environment
 import numpy as np
 import pytest
 
+@pytest.fixture
+def RPSenv():
+    import gym
+    import gym_rock_paper_scissors
+    return gym.make('RockPaperScissors-v0')
+
 
 @pytest.fixture
-def env():
-    class Env():
-        state_space_size  = 5
-        action_space_size = 5
-        hashing_function = lambda x: x
-    return Env()
+def RPSTask(RPSenv):
+    return parse_gym_environment(RPSenv)
 
 
-def test_agents_instantiation(env):
-    tql = build_TabularQ_Agent()
-    dqn = build_DQN_Agent()
-    ddpg = build_DDPG_Agent()
+@pytest.fixture
+def ppo_config_dict():
+    config = dict()
+    config['discount'] = 0.99
+    config['use_gae'] = False
+    config['use_cuda'] = False
+    config['gae_tau'] = 0.95
+    config['entropy_weight'] = 0.01
+    config['gradient_clip'] = 5
+    config['optimization_epochs'] = 10
+    config['mini_batch_size'] = 256
+    config['ppo_ratio_clip'] = 0.2
+    config['learning_rate'] = 3.0e-4
+    config['adam_eps'] = 1.0e-5
+    config['horizon'] = 1024
+    return config
 
-def test_agent_hooking(env=None):
-    tql = build_TabularQ_Agent()
-    dqn = build_DQN_Agent()
-    ddpg = build_DDPG_Agent()
-    
-    path_tql = './data/tql'
-    ah_tql = AgentHook(tql,path=path_tql)
+@pytest.fixture
+def dqn_config_dict():
+    config = dict()
+    config['batch_size'] = 32
+    config['gamma'] = 0.99
+    config['tau'] = 1.0e-3
+    config['learning_rate'] = 1.0e-3
+    config['epsstart'] = 0.8
+    config['epsend'] = 0.05
+    config['epsdecay'] = 1.0e3
+    config['double'] = False
+    config['dueling'] = False
+    config['use_cuda'] = False
+    config['use_PER'] = False
+    config['PER_alpha'] = 0.07
+    config['min_memory'] = 5.0e1
+    config['memoryCapacity'] = 25.0e3
+    config['nbrTrainIteration'] = 32
+    return config
+
+def test_agent_hooking(RPSenv, RPSTask, dqn_config_dict,ppo_config_dict):
+    #tql = build_TabularQ_Agent()
+    dqn = build_DQN_Agent(RPSTask,dqn_config_dict)
+    #ddpg = build_DDPG_Agent(RPSTask,ppo_config_dict)
+
+    #path_tql = './data/tql'
+    #ah_tql = AgentHook(tql,path=path_tql)
     path_dqn = './data/dqn'
     ah_dqn = AgentHook(dqn,path=path_dqn)
-    path_ddpg = './data/ddpg'
-    ah_ddpg = AgentHook(ddpg,path=path_ddpg)
+    #path_ddpg = './data/ddpg'
+    #ah_ddpg = AgentHook(ddpg,path=path_ddpg)
 
-    tql2 = ah_tql()
+    #tql2 = ah_tql()
     dqn2 = ah_dqn()
-    ddpg2 = ah_ddpg()
+    #ddpg2 = ah_ddpg()
 
-    assert tql == tql2 
+    #assert tql == tql2 
+    
     tests = [dqn.kwargs[el] == dqn2.kwargs[el] for el in dqn.kwargs if not('model' in el) ]
-    for el in tests : assert(el)
-    tests = [ddpg.kwargs[el] == ddpg2.kwargs[el] for el in ddpg.kwargs if not('model' in el or 'process' in el) ]
-    for el in tests : assert(el)
+    import ipdb; ipdb.set_trace()
+    assert(all(tests))
+    
+    #tests = [ddpg.kwargs[el] == ddpg2.kwargs[el] for el in ddpg.kwargs if not('model' in el or 'process' in el) ]
+    #for el in tests : assert(el)
     
