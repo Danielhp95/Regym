79036654b72ec092348dd8dc456a715880229767
Daniel Hernandez <danielhp95@gmail.com>
2019-02-28 15:54:07 +0000

    Added experimental script for testing sending over queues pytorch models

---
+++ new_ideas/multiprocess_pickle_torch_model.py
@@ -0,0 +1,102 @@
+import os
+import sys
+sys.path.append(os.path.abspath('..'))
+
+from torch.multiprocessing import Process, Queue
+from rl_algorithms import build_PPO_Agent, build_DQN_Agent
+from environments import parse_gym_environment
+from multiagent_loops.simultaneous_action_rl_loop import run_episode
+
+
+def RPSenv():
+    import gym
+    import gym_rock_paper_scissors
+    return gym.make('RockPaperScissors-v0')
+
+
+def RPSTask(env):
+    return parse_gym_environment(env)
+
+
+def ppo_config_dict():
+    config = dict()
+    config['discount'] = 0.99
+    config['use_gae'] = False
+    config['use_cuda'] = False
+    config['gae_tau'] = 0.95
+    config['entropy_weight'] = 0.01
+    config['gradient_clip'] = 5
+    config['optimization_epochs'] = 10
+    config['mini_batch_size'] = 256
+    config['ppo_ratio_clip'] = 0.2
+    config['learning_rate'] = 3.0e-4
+    config['adam_eps'] = 1.0e-5
+    config['horizon'] = 1024
+    return config
+
+
+def dqn_config_dict():
+    config = dict()
+    config['batch_size'] = 32
+    config['gamma'] = 0.99
+    config['tau'] = 1.0e-3
+    config['learning_rate'] = 1.0e-3
+    config['epsstart'] = 0.8
+    config['epsend'] = 0.05
+    config['epsdecay'] = 1.0e3
+    config['double'] = False
+    config['dueling'] = False
+    config['use_cuda'] = False
+    config['use_PER'] = False
+    config['PER_alpha'] = 0.07
+    config['min_memory'] = 5.0e1
+    config['memoryCapacity'] = 25.0e3
+    config['nbrTrainIteration'] = 32
+    return config
+
+
+def self_play_training_process(queue, end_queue):
+    env = RPSenv()
+
+    agent = queue.get()
+    agent2 = queue.get()
+
+    print('Began')
+    run_episode(env, [agent, agent2], training=False)
+    print('Lol')
+
+    end_queue.put(agent.algorithm.model)
+    end_queue.put(agent.algorithm.optimizer)
+    end_queue.put(agent.algorithm)
+    end_queue.put(agent.state_preprocessing)
+    end_queue.put(agent.current_prediction)
+    while True: pass
+
+
+if __name__ == '__main__':
+    import torch
+    torch.multiprocessing.set_start_method('forkserver')
+
+    agent  = build_PPO_Agent(RPSTask(RPSenv()), ppo_config_dict())
+    agent2  = build_PPO_Agent(RPSTask(RPSenv()), ppo_config_dict())
+    # agent2 = build_DQN_Agent(RPSTask(RPSenv()), dqn_config_dict())
+    agent_queue = Queue()
+    end_queue = Queue()
+
+    p = Process(target=self_play_training_process, args=(agent_queue, end_queue))
+
+    agent_queue.put(agent)
+    agent_queue.put(agent2)
+    p.start()
+    print(end_queue.qsize())
+
+    end_model = end_queue.get()
+    print(end_model)
+    end_optimizer = end_queue.get()
+    print(end_optimizer)
+    algorithm = end_queue.get()
+    print(algorithm)
+    state_pre = end_queue.get()
+    print(state_pre)
+    #agent = end_queue.get()
+    #print(agent)
