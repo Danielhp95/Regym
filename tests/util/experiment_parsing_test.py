005e91e38e536632547ea33906bdcefeb812e7a8
Daniel Hernandez <danielhp95@gmail.com>
2019-03-07 13:04:50 +0000

    Configuration is parsed on partal match with algorithm name

---
+++ tests/util/experiment_parsing_test.py
@@ -0,0 +1,33 @@
+import os
+import sys
+sys.path.append(os.path.abspath('../../'))
+
+import yaml
+import pytest
+
+from util import experiment_parsing
+
+
+@pytest.fixture
+def experiment_config():
+    experiment_config = '''
+    experiment:
+        algorithms: ['ppo', 'tabularqlearning']
+    agents:
+        deepqlearning:
+            placeholder: None
+        ppo:
+            placeholder: None
+        ppo_dani:
+            placeholder: None
+    '''
+    return yaml.load(experiment_config)
+
+
+def test_caca(experiment_config):
+
+    filtered_agent_config = experiment_parsing.filter_relevant_agent_configurations(experiment_config['experiment'], experiment_config['agents'])
+    assert 'ppo' in filtered_agent_config
+    assert 'ppo_dani' in filtered_agent_config
+    assert 'deeplearning' not in filtered_agent_config
+    assert 'tabularqlearning' not in filtered_agent_config
