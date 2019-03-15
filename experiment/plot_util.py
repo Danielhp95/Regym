1c28c172e921f04a9b7848d74d1c6d6d0e06da7f
Denamganai Kevin <denamganai.kevin@gmail.com>
2019-03-15 00:31:07 +0000

    added the computation of violinplots of the winrates per agents...

--- experiment/plot_util.py
+++ experiment/plot_util.py
@@ -10,6 +10,9 @@ import matplotlib
 matplotlib.use("agg")
 from matplotlib import pyplot as plt
 
+import seaborn as sns 
+import pandas as pd 
+
 
 def create_plots(experiment_directory, number_of_runs):
     force_create_directory('{}/plots'.format(experiment_directory))
@@ -27,18 +30,36 @@ def create_plots(experiment_directory, number_of_runs):
                             aggregated_benchmark_winrate_dict,
                             target_dir='{}/plots'.format(experiment_directory))
 
+def create_violinplots(experiment_directory, number_of_runs):
+    force_create_directory('{}/violinplots'.format(experiment_directory))
+
+    aggregated_benchmark_winrate_dict = {'agent':[],'opponent':[],'iteration':[],'winrate':[]}
+    for run_id in tqdm(range(number_of_runs)):
+        new_winrate_dict = violinplot_single_run_benchmark_winrate(run_id=run_id,
+                                           source_dir='{}/run-{}/confusion_matrices'.format(experiment_directory, run_id))
+        
+        update_benchmark_winrates_violinplot(aggregated_benchmark_winrate_dict, new_winrate_dict)
+        
+    force_create_directory('{}/violinplots'.format(experiment_directory))
+    create_aggregated_benchmark_winrate_violinplots(aggregated_benchmark_winrate_dict,
+                            target_dir='{}/violinplots'.format(experiment_directory))
 
 def update_aggregated_statistics(aggregated_episodic_reward_dict, aggregated_benchmark_winrate_dict,
                                  new_episodic_reward_dict, new_winrate_dict):
     update_episodic_reward(aggregated_episodic_reward_dict, new_episodic_reward_dict)
     update_benchmark_winrates(aggregated_benchmark_winrate_dict, new_winrate_dict)
-
-
+    
 def update_benchmark_winrates(old_dict, new_dict):
     for name, (iterations, winrates) in new_dict.items():
         if name in old_dict: old_dict[name].append((iterations, winrates))
         if name not in old_dict: old_dict[name] = [(iterations, winrates)]
 
+def update_benchmark_winrates_violinplot(old_dict, new_dict):
+    for key in new_dict:
+        if key not in old_dict: 
+            old_dict[key] = []
+        for el in new_dict[key]:
+            old_dict[key].append(el)
 
 def update_episodic_reward(old_dict, new_dict):
     for name, episodic_rewards in new_dict.items():
@@ -81,7 +102,6 @@ def create_aggregated_benchmark_winrate_plot(winrate_dict, target_dir):
         means, standard_deviations = winrates.mean(axis=0), winrates.std(axis=0)
 
         plt.errorbar(iterations, means, standard_deviations, marker='o', label=name)
-
         y_max = 1
         plt.xticks(iterations)
         plt.yticks(np.arange(0, y_max + y_max*0.1, y_max * 0.1)) # Watch out when changing y_max
@@ -94,6 +114,20 @@ def create_aggregated_benchmark_winrate_plot(winrate_dict, target_dir):
     plt.tight_layout()
     plt.savefig('{}/benchmark_winrates.eps'.format(target_dir), format='eps')
     plt.close()
+
+def create_aggregated_benchmark_winrate_violinplots(winrate_dict, target_dir):
+    agent_names = []
+    agent_names = [ name for name in winrate_dict['agent'] if name not in agent_names]
+    per_agent_winrate_dict = { agent_name:{key:[] for key in winrate_dict.keys()} for agent_name in agent_names  }
+    nbr_items = len(winrate_dict['winrate'])
+    for idx in range(nbr_items):
+        for key in winrate_dict.keys():
+            per_agent_winrate_dict[ winrate_dict['agent'][idx] ][key].append( winrate_dict[key][idx] )
+
+    axes = []
+    for agent_name in per_agent_winrate_dict.keys():
+        ax = create_single_agent_violinplot(agent_name, target_dir, per_agent_winrate_dict[agent_name] )
+        axes.append(ax)
 
 
 def plot_single_run(run_id, source_dir):
@@ -108,6 +142,10 @@ def plot_single_run(run_id, source_dir):
     plt.close()
     return episodic_reward_dict, benchmark_winrate_dict
 
+def violinplot_single_run_benchmark_winrate(run_id, source_dir):
+    axis_labels = [axis for axis in read_labels_from_file('{}/legend.txt'.format(source_dir))]
+    benchmark_winrate_dict = create_benchmark_winrate_dict(source_dir=source_dir,axis_labels=axis_labels)
+    return benchmark_winrate_dict
 
 def create_confusion_matrix_heatmaps(source_dir, target_dir):
     axis_labels = [axis.replace('-', '\n') for axis in read_labels_from_file('{}/legend.txt'.format(source_dir))]
@@ -116,7 +154,6 @@ def create_confusion_matrix_heatmaps(source_dir, target_dir):
         if f == '{}/legend.txt'.format(source_dir): continue
         create_single_heatmap(f, target_dir, axis_labels)
         plt.close()
-
 
 def create_single_heatmap(source, target_dir, axis_labels):
     file_name = get_file_name_from_full_path(source)
@@ -144,6 +181,20 @@ def create_single_heatmap(source, target_dir, axis_labels):
 
     fig.savefig('{}/heatmap-{}.eps'.format(target_dir, file_name), format='eps')
 
+def create_single_agent_violinplot(agent_name, target_dir, data):
+    df = pd.DataFrame(data=data).sort_values('iteration')
+    df = df[ df.opponent != agent_name ]
+
+    fig, ax = plt.subplots()
+    ax = sns.violinplot(x="iteration", y="winrate", hue="opponent", data=df, palette="muted", ax=ax, cut=0, width=1.1, linewidth=0.5, saturation=0.8, inner='box')
+    
+    plt.title('Head to head winrates against all opponents\nfor policy: {}'.format(agent_name))
+    plt.tight_layout()
+
+    fig.savefig('{}/violinplot-{}.eps'.format(target_dir, agent_name), format='eps')
+    plt.close()
+
+    return ax 
 
 def create_average_winrate_graph(source_dir, target_dir):
     benchmark_winrate_dict = {}
@@ -169,6 +220,20 @@ def create_average_winrate_graph(source_dir, target_dir):
 
     return benchmark_winrate_dict
 
+def create_benchmark_winrate_dict(source_dir, axis_labels):
+    benchmark_winrate_dict = {'iteration':[], 'winrate':[], 'opponent':[],"agent":[]}
+    for f in all_files_in_directory(source_dir):
+        if f == '{}/legend.txt'.format(source_dir): continue
+        iteration = get_iteration_from_full_path(f)
+        file_content = np.loadtxt(open(f, 'rb'), delimiter=', ')
+        nbr_labels = len(axis_labels)
+        for idx_line_label, line_label in enumerate(axis_labels):
+            for idx_col_label, col_label in enumerate(axis_labels):
+                benchmark_winrate_dict['iteration'].append(iteration)
+                benchmark_winrate_dict['winrate'].append(file_content[idx_line_label, idx_col_label])
+                benchmark_winrate_dict['opponent'].append(col_label)
+                benchmark_winrate_dict['agent'].append(line_label)
+    return benchmark_winrate_dict
 
 def create_individual_episodic_reward_graph(source_dir, target_dir):
     episodic_reward_dict = {}
@@ -200,6 +265,8 @@ def all_files_in_directory(directory):
 def get_file_name_from_full_path(filename):
     return os.path.splitext(filename)[0].split('/')[-1]
 
+def get_iteration_from_full_path(filename):
+    return int(os.path.splitext(filename)[0].split('/')[-1].split('-')[-1])
 
 def read_labels_from_file(path):
     with open(path, 'r') as f:
@@ -225,4 +292,5 @@ if __name__ == '__main__':
     docopt_options = docopt(_USAGE)
     source_dir = docopt_options['--source']
     number_of_runs = len([f for f in os.listdir('./' + source_dir) if f.startswith('run')])
-    create_plots(experiment_directory=source_dir,  number_of_runs=number_of_runs)
+    #create_plots(experiment_directory=source_dir,  number_of_runs=number_of_runs)
+    create_violinplots(experiment_directory=source_dir, number_of_runs=number_of_runs)
