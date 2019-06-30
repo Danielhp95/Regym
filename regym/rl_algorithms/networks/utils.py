5b24df5033ddf5a960897b8d9ab90bda639681e4
Sarios <danielhp95@gmail.com>
2019-06-30 21:26:02 +0100

    Added function to calculate size of output layer for a torch.nn.Module

--- regym/rl_algorithms/networks/utils.py
+++ regym/rl_algorithms/networks/utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import torch.autograd
 
 
 class PreprocessFunction():
@@ -24,3 +25,15 @@ def random_sample(indices, batch_size):
     remainder = len(indices) % batch_size
     if remainder:
         yield indices[-remainder:]
+
+
+def output_size_for_model(model, input_shape):
+    '''
+    Computes the size of the last layer of the :param model:
+    which takes an input of shape :param input_shape:.
+
+    :param model: torch.nn.Module model which takes an input of shape :param input_shape:
+    :param input_shape: Shape of the input of the
+    :returns: size of the flattened output torch.Tensor
+    '''
+    return model(torch.autograd.Variable(torch.zeros(1, *input_shape))).view(1, -1).size(1)
