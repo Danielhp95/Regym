00bfd36cdd8eb0532cb31c093a7d8ac6f9025a9c
Sarios <danielhp95@gmail.com>
2019-07-02 12:24:38 +0100

    added integration test with OTC. OTC is not included in regym's codebase

---
+++ regym/tests/integration/test_run_i2a_unity_otc.py
@@ -0,0 +1,12 @@
+from regym.rl_algorithms.agents import build_I2A_Agent
+from regym.rl_loops.singleagent_loops import rl_loop
+from test_fixtures import i2a_config_dict, otc_task
+
+
+def test_can_run_episodes_inside_otc_env(i2a_config_dict, otc_task):
+    agent_i2a = build_I2A_Agent(config=i2a_config_dict, task=otc_task, agent_name='TestI2A')
+    trajectory = rl_loop.run_episode(otc_task.env, agent_i2a, training=False)
+    otc_task.env.close()
+
+    assert trajectory is not None
+    assert isinstance(trajectory, list)
