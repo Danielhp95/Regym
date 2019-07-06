00ce7a4668aa1a743fe7d18c991cf3a3ee5da45b
Kevin Denamganai <denamganai.kevin@gmail.com>
2019-07-06 21:51:43 +0100

    preprocess functions...

--- regym/rl_algorithms/networks/utils.py
+++ regym/rl_algorithms/networks/utils.py
@@ -4,12 +4,17 @@ import torchvision.transforms as T
 import numpy as np
 
 
-def PreprocessFunction(x, use_cuda=False):
+def PreprocessFunctionConcatenate(x, use_cuda=False):
     x = np.concatenate(x, axis=None)
     if use_cuda:
         return torch.from_numpy(x).unsqueeze(0).type(torch.cuda.FloatTensor)
     return torch.from_numpy(x).unsqueeze(0).type(torch.FloatTensor)
 
+def PreprocessFunction(x, use_cuda=False):
+    if use_cuda:
+        return torch.from_numpy(x).type(torch.cuda.FloatTensor)
+    else:
+        return torch.from_numpy(x).type(torch.FloatTensor)
 
 def ResizeCNNPreprocessFunction(x, size, use_cuda=False, normalize_rgb_values=True):
     '''
@@ -54,17 +59,6 @@ def CNNPreprocessFunction(x, use_cuda=False, normalize_rgb_values=True):
         return torch.from_numpy(x).unsqueeze(0).type(torch.cuda.FloatTensor)
     return torch.from_numpy(x).unsqueeze(0).type(torch.FloatTensor)
 
-class PreprocessFunction():
-
-    def __init__(self, state_space_size, use_cuda=False):
-        self.state_space_size = state_space_size
-        self.use_cuda = use_cuda
-
-    def __call__(self, x):
-        if self.use_cuda:
-            return torch.from_numpy(x).type(torch.cuda.FloatTensor)
-        else:
-            return torch.from_numpy(x).type(torch.FloatTensor)
 
 def random_sample(indices, batch_size):
     '''
