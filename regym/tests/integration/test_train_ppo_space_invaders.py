ae78c4356d0306d2faa6be7037771990da4ffcb3
Sarios <danielhp95@gmail.com>
2019-07-09 17:33:48 +0100

    Changed test names to make them actual pytests instead of normal scripts. NOTE: these should be in tests/rl_algorithms not in integration

--- regym/tests/integration/test_train_ppo_space_invaders.py
+++ regym/tests/integration/test_train_ppo_space_invaders.py
@@ -3,8 +3,8 @@ from regym.rl_loops.singleagent_loops import rl_loop
 from test_fixtures import ppo_config_dict, si_task
 from tqdm import tqdm
 from tensorboardX import SummaryWriter
-import os 
-import math 
+import os
+import math
 import torch
 
 def test_train_ppo(ppo_config_dict, si_task):
@@ -16,7 +16,7 @@ def test_train_ppo(ppo_config_dict, si_task):
     save_path = os.path.join(logdir,'./ppo.agent')
 
     agent = build_PPO_Agent(config=ppo_config_dict, task=task, agent_name='TestPPO_RNN_SI')
-    nbr_episodes = 1e6
+    nbr_episodes = 1e2
     max_episode_length = math.inf
 
     for i in tqdm(range(int(nbr_episodes))):
@@ -25,11 +25,8 @@ def test_train_ppo(ppo_config_dict, si_task):
 
         total_return = sum([ t[2] for t in  trajectory])
         sum_writer.add_scalar('Training/TotalReturn', total_return, i)
-        
+
     task.env.close()
 
     assert trajectory is not None
     assert isinstance(trajectory, list)
-
-if __name__ == '__main__':
-    test_train_ppo(ppo_config_dict(),si_task())
