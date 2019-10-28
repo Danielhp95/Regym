18752cbca045a7e3c42574bc340dd9536e352498
Daniel Hernandez <danielhp95@gmail.com>
2019-10-28 20:27:07 +0000

    Implemented and tested logic to compute a winrate matrix for an environment given a population of agents

--- regym/game_theory/__init__.py
+++ regym/game_theory/__init__.py
@@ -1,2 +1,3 @@
 from .solve_zero_sum_game import solve_zero_sum_game
-from .compute_nash_averaging import compute_nash_averaging
+from .compute_nash_averaging import compute_nash_averaging, compute_nash_average
+from .compute_winrate_matrix_metagame import compute_winrate_matrix_metagame
