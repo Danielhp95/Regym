13451f0ec5bcf8d5eff306b81e80b9d7578020be
Sarios <danielhp95@gmail.com>
2019-11-06 23:38:34 +0000

    Added tests for monoaction games and rock paper scissors

--- regym/tests/game_theory/test_compute_winrate_matrix_metagame.py
+++ regym/tests/game_theory/test_compute_winrate_matrix_metagame.py
@@ -1,7 +1,7 @@
 import pytest
-import unittest
 import numpy as np
 
+from regym.rl_algorithms.agents import rockAgent, paperAgent, scissorsAgent
 from regym.game_theory import compute_winrate_matrix_metagame
 from regym.rl_algorithms import build_Reinforce_Agent, build_PPO_Agent
 
@@ -14,35 +14,65 @@ def test_for_none_population_raises_valueerror(RPSTask):
                                             episodes_per_matchup=10,
                                             env=RPSTask.env)
 
+
 def test_for_empty_population_raises_valueerror(RPSTask):
     with pytest.raises(ValueError) as _:
         _ = compute_winrate_matrix_metagame(population=[],
                                             episodes_per_matchup=10,
                                             env=RPSTask.env)
 
+
 def test_for_negative_or_zero_episodes_per_matchup_raises_valueerror(RPSTask):
     with pytest.raises(ValueError) as _:
         # TODO: is it worth it to pass an agent?
-        _ = compute_winrate_matrix_metagame(population=[1,2,3],
+        _ = compute_winrate_matrix_metagame(population=[1, 2, 3],
                                             episodes_per_matchup=-1,
                                             env=RPSTask.env)
 
     with pytest.raises(ValueError) as _:
         # TODO: is it worth it to pass an agent?
-        _ = compute_winrate_matrix_metagame(population=[1,2,3],
+        _ = compute_winrate_matrix_metagame(population=[1, 2, 3],
                                             episodes_per_matchup=0,
                                             env=RPSTask.env)
+
 
 def test_for_none_environment_raises_valueerror(RPSTask):
     with pytest.raises(ValueError) as _:
         # TODO: is it worth it to pass an agent?
-        _ = compute_winrate_matrix_metagame(population=[1,2,3],
+        _ = compute_winrate_matrix_metagame(population=[1, 2, 3],
                                             episodes_per_matchup=10,
                                             env=None)
 
 # TODO: add test to check if environment is multi_agent or single_agent?
 
-def test(ppo_config_dict, RPSTask):
+
+def test_single_agent_population(RPSTask):
+    population = [rockAgent]
+    expected_winrate_matrix = np.array([[0.5]])
+
+    actual_winrate_matrix = compute_winrate_matrix_metagame(population=population,
+                                                            episodes_per_matchup=1,
+                                                            env=RPSTask.env,
+                                                            num_workers=1)
+
+    np.testing.assert_array_equal(expected_winrate_matrix, actual_winrate_matrix)
+
+
+def test_can_compute_rock_paper_scissors_metagame(RPSTask):
+    population = [rockAgent, paperAgent, scissorsAgent]
+    expected_winrate_matrix = np.array([[0.5, 0., 1.],
+                                        [1., 0.5, 0.],
+                                        [0., 1., 0.5]])
+
+    actual_winrate_matrix = compute_winrate_matrix_metagame(population=population,
+                                                            episodes_per_matchup=5,
+                                                            env=RPSTask.env,
+                                                            num_workers=1)
+
+    np.testing.assert_array_equal(expected_winrate_matrix, actual_winrate_matrix)
+
+
+def test_integration_ppo_rock_paper_scissors(ppo_config_dict, RPSTask):
     population = [build_PPO_Agent(RPSTask, ppo_config_dict, 'Test-1'),
                   build_PPO_Agent(RPSTask, ppo_config_dict.copy(), 'Test-2')]
     winrate_matrix_metagame = compute_winrate_matrix_metagame(population=population,
