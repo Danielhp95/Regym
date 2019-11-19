c113e7881012bc5f7d9ffdf599eb03462b98eb1e
Sarios <danielhp95@gmail.com>
2019-11-19 20:23:13 +0000

    Added f'' to error message to display faulting environment name

--- regym/environments/parse_environment.py
+++ regym/environments/parse_environment.py
@@ -20,7 +20,7 @@ def generate_task(env_name):
     if is_gym_environment and is_unity_environment: raise ValueError(f'{env_name} exists as both a Gym and an Unity environment. Rename Unity environment to remove duplicate problem.')
     if is_gym_environment: return parse_gym_environment(gym.make(env_name))
     if is_unity_environment: return parse_unity_environment(env_name)
-    else: raise ValueError('Environment \'{env_name}\' was not recognized as either a Gym nor a Unity environment')
+    else: raise ValueError(f'Environment \'{env_name}\' was not recognized as either a Gym nor a Unity environment')
 
 
 def check_for_unity_executable(env_name):
