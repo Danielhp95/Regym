79e4e3db15afab6230e1f8988d3aa11d6efb2a66
Sarios <danielhp95@gmail.com>
2019-11-25 17:36:54 +0000

    added psro!

--- regym/training_schemes/psro.py
+++ regym/training_schemes/psro.py
@@ -4,6 +4,9 @@ http://papers.nips.cc/paper/7007-a-unified-game-theoretic-approach-to-multiagent
 
 TODO: difference between PSRO which takes 3 separate stages and our method, which is an online method.
 '''
+from recordclass import recordclass
+import logging
+import time
 from typing import Callable, List
 from itertools import product
 import numpy as np
@@ -17,7 +20,12 @@ from regym.environments import generate_task
 
 class PSRONashResponse():
 
-    # TODO: in Self-play training, pass agent index for the policy being trained.
+    IterationStatistics = recordclass('IterationStatistics', [
+                                      'total_elapsed_episodes',
+                                      'current_iteration_elapsed_episodes',
+                                      'menagerie_picks',
+                                      'meta_game_solution'])
+
     def __init__(self,
                  env_id: str,
                  meta_game_solver: Callable = lambda winrate_matrix: compute_nash_averaging(winrate_matrix, perform_logodds_transformation=True)[0],
@@ -39,10 +47,12 @@ class PSRONashResponse():
                                                   decide whether the currently training agent
                                                   has converged to a best response.
         '''
+        self.name = 'PSRO_M=maxent-Nash_O=BestResponse'
+        self.logger = logging.getLogger(self.name)
+        self.logger.setLevel(logging.DEBUG)
         self.check_parameter_validity(env_id, threshold_best_response,
                                       benchmarking_episodes,
                                       match_outcome_rolling_window_size)
-        self.name = 'PSRO_M=maxent-Nash_O=BestResponse'
         self.env_id = env_id
 
         self.meta_game_solver = meta_game_solver
@@ -56,16 +66,24 @@ class PSRONashResponse():
 
         self.benchmarking_episodes = benchmarking_episodes
 
+        self.statistics = [self.IterationStatistics(0, 0, [0], np.nan)]
+
     def opponent_sampling_distribution(self, menagerie, training_agent):
         '''
         :param menagerie: archive of agents selected by the curator and the potential opponents
         :param training_agent: Agent currently being trained
         '''
-        if len(self.menagerie) == 0:
-            self.add_agent_to_menagerie(training_agent)
-            self.meta_game = np.array([[0.5]])
-        meta_game_solution_probabilities = self.get_meta_game_solution(update=True)
-        return [np.random.choice(self.menagerie, p=meta_game_solution_probabilities)]
+        if len(menagerie) == 0 and len(self.menagerie) == 0:
+            self.init_meta_game_and_solution(training_agent)
+        sampled_index = np.random.choice([i for i in range(len(self.menagerie))],
+                                         p=self.meta_game_solution)
+        self.statistics[-1].menagerie_picks[sampled_index] += 1
+        return [self.menagerie[sampled_index]]
+
+    def init_meta_game_and_solution(self, training_agent):
+        self.add_agent_to_menagerie(training_agent)
+        self.meta_game = np.array([[0.5]])
+        self.meta_game_solution = np.array([1.0])
 
     def curator(self, menagerie, training_agent, episode_trajectory,
                 training_agent_index, candidate_save_path):
@@ -74,11 +92,23 @@ class PSRONashResponse():
         :param training_agent: AgentHook of the Agent currently being trained
         :returns: menagerie to be used in the next training episode.
         '''
+        self.statistics[-1].total_elapsed_episodes += 1
+        self.statistics[-1].current_iteration_elapsed_episodes += 1
+
         self.update_rolling_winrates(episode_trajectory, training_agent_index)
-        if (sum(self.match_outcome_rolling_window) / self.match_outcome_rolling_window_size) >= self.threshold_best_response:
+        if self.has_policy_converged():
             self.add_agent_to_menagerie(training_agent, candidate_save_path)
-            self.match_outcome_rolling_window = []
             self.update_meta_game()
+            self.update_meta_game_solution()
+            self.match_outcome_rolling_window = []
+            self.statistics += [self.create_new_iteration_statistics(self.statistics[-1])]
+            self.statistics[-1].meta_game_solution = self.meta_game_solution
+        return self.menagerie
+
+    def has_policy_converged(self):
+        current_winrate = (sum(self.match_outcome_rolling_window) \
+                           / self.match_outcome_rolling_window_size)
+        return current_winrate >= self.threshold_best_response
 
     def update_rolling_winrates(self, episode_trajectory, training_agent_index):
         winner_index = extract_winner(episode_trajectory)
@@ -87,12 +117,17 @@ class PSRONashResponse():
             self.match_outcome_rolling_window.pop(0)
         self.match_outcome_rolling_window.append(victory)
 
-    def get_meta_game_solution(self, update=False):
-        if self.meta_game_solution is None or update:
-            self.meta_game_solution = self.meta_game_solver(self.meta_game)
+    def update_meta_game_solution(self, update=False):
+        self.logger.debug(f'START: Solving metagame. Size: {len(self.menagerie)}')
+        start_time = time.time()
+        self.meta_game_solution = self.meta_game_solver(self.meta_game)
+        time_elapsed = time.time() - start_time
+        self.logger.debug(f'FINISH: Solving metagame. time: {time_elapsed}')
         return self.meta_game_solution
 
     def update_meta_game(self):
+        self.logger.debug(f'START: updating metagame. Size: {len(self.menagerie)}')
+        start_time = time.time()
         number_old_policies = len(self.menagerie) - 1
         updated_meta_game = np.full(((len(self.menagerie)), len(self.menagerie)),
                                     np.nan)
@@ -103,6 +138,8 @@ class PSRONashResponse():
         self.fill_meta_game_missing_entries(self.menagerie, updated_meta_game, self.benchmarking_episodes,
                                             self.env_id)
         self.meta_game = updated_meta_game
+        time_elapsed = time.time() - start_time
+        self.logger.debug(f'FINISH: updating metagame. time: {time_elapsed}')
         return updated_meta_game
 
     def fill_meta_game_missing_entries(self, policies: List,
@@ -125,6 +162,11 @@ class PSRONashResponse():
         if candidate_save_path is not None:
             AgentHook(training_agent, save_path=candidate_save_path)
         self.menagerie.append(training_agent.clone(training=False))
+
+    def create_new_iteration_statistics(self, last_iteration_statistics):
+        return self.IterationStatistics(last_iteration_statistics.total_elapsed_episodes,
+                                        0, [0] * len(self.menagerie),
+                                        self.meta_game_solution)
 
     def check_parameter_validity(self, env_id, threshold_best_response,
                                  benchmarking_episodes,
