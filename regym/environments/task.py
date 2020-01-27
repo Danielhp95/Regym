81f70d5968b7476cdc0b294521d655f34cb75216
Sarios <danielhp95@gmail.com>
2020-01-27 14:32:10 +0000

    Minimal documentation added to EnvType

--- regym/environments/task.py
+++ regym/environments/task.py
@@ -8,6 +8,11 @@ from regym.rl_loops.multiagent_loops import simultaneous_action_rl_loop
 
 
 class EnvType(Enum):
+    '''
+    Enumerator representing what kind of environment a task will deal with.
+    Useful because different environments (simulatenous vs sequential) require
+    a different underlying mathematical construct to simulate an episode
+    '''
     SINGLE_AGENT = 'single-agent'
     MULTIAGENT_SIMULTANEOUS_ACTION = 'multiagent-simultaneous'
     MULTIAGENT_SEQUENTIAL_ACTION = 'multiagent-sequential'
