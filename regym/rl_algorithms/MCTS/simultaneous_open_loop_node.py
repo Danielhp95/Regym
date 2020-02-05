a43b399dd7e443bb471fccff0ab9f0cc285fc975
Sarios <danielhp95@gmail.com>
2020-02-05 12:52:27 +0000

    Added documentation

--- regym/rl_algorithms/MCTS/simultaneous_open_loop_node.py
+++ regym/rl_algorithms/MCTS/simultaneous_open_loop_node.py
@@ -1,48 +1,69 @@
+from typing import List
 from functools import reduce
+import gym
 
 
 class SimultaneousOpenLoopNode:
     """
-        An Open-loop approach keeps only one state representation at the root node of the game tree being computed by MCTS, and each of the edges represent an action. As the game tree is being traversed, the actions stored in the nodes being traversed are used as input to the model of the game. Thus the initial state stored at the root of the tree changes according to the actions selected during the game tree traversal. Once an MCTS iteration is over, this stated modified during the tree traversal is discarded, and a new clone of the state of the root node is created. Furthermore, we don't store the intermediate states obtained during the traversal. This means that we are esentially throwing away the computation performed by the model as soon as we don't need it.
+    Open loop tree implementation apt for MCTS in SIMULTANEOUS tasks.
+    An Open-loop approach keeps only one state representation at the root node
+    of the game tree being computed by MCTS. Except the root node, each node
+    stores which action was executed w.r.t to its parent node. As the game
+    tree is being traversed, the actions stored in the nodes being traversed
+    are used as input to the model of the game. Thus the initial state stored
+    at the root of the tree changes according to the actions selected during
+    the game tree traversal.
+    Once an MCTS iteration is over, this stated modified during the tree
+    traversal is discarded, and a new clone of the state of the root node
+    is created.
 
 
-        Node of a game tree. A tree is a connected acyclic graph.
-        Note: self.wins is from the perspective of playerJustMoved.
 
-        TODO: write assumptions made over the interface of the underlying
-              OpenAIGym environment. THIS IS SUPER IMPORTANT
+    TODO: write assumptions made over the interface of the underlying
+          OpenAIGym environment. THIS IS SUPER IMPORTANT
     """
 
-    def __init__(self, perspective_player: int, move=None, parent=None, state=None):
-        self.move = move  # Move that was taken to reach this game state
+    def __init__(self, perspective_player: int, move: int = None,
+                 parent=None, state: gym.Env = None):
+        self.move = move           # Move that was taken to reach this game state
         self.parent_node = parent  # "None" for the root node
         self.child_nodes = []
         self.perspective_player = perspective_player
 
-        self.wins = 0
+        self.wins = 0  # Note: self.wins is from the perspective of `perspective_player`.
+
         self.visits = 0
-        self.is_chance_node = state is None
+        self.is_chance_node = state is None  # i.e `perspective_player` does not act in this node
         self.untried_moves = state.get_moves(self.perspective_player) if not self.is_chance_node else None
 
-
-    def is_fully_expanded(self):
+    def is_fully_expanded(self) -> bool:
         assert not self.is_chance_node
         return self.untried_moves == []
 
-    def descend_and_expand(self, moves, whereWeEndUpAfterwards):
+    def descend_and_expand(self, moves: List[int], state: gym.Env):
+        '''
+        Descends the tree, of which `self` is a node, according
+        to :param: moves. If there are no deeper nodes containing
+        the moves from :param: moves, then the tree is expanded
+        to contain them.
+
+        :params state: Game state
+        :params moves: List of moves coming from selection phase,
+                       one for each player
+        :returns: Child node, linked backwards to `self` by :param: moves
+        '''
         # we iterate through actions and move to the next node
         # if none exist then we expand one and continue
-        def descend_tree(node, move_taken, chance_node=True):
-            matching_nodes = list(filter(lambda n: n.move == move_taken, node.child_nodes))
-            assert len(matching_nodes) == 0 or len(matching_nodes) == 1
-            if len(matching_nodes) == 0:
-                # Node must be expanded
-                # For all other players we add a chance node, and we end on a decision node (state required)
-                node = node.add_child(move_taken, whereWeEndUpAfterwards if not chance_node else None)
-            else:
-                node = matching_nodes.pop(0)
-            return node
+        def descend_tree(node, move_taken: int, chance_node=True):
+            matching_node = list(filter(lambda n: n.move == move_taken, node.child_nodes))
+            # Node must be expanded
+            if matching_node == []: return node.add_child(
+                                                      move_taken,
+                                                      state if not chance_node else None)
+            # Node exists
+            else: return matching_node.pop(0)
 
+        # The first action that must be taken is that from `self.perspective_player`
         node = descend_tree(self, moves[self.perspective_player])
         last_index = len(moves) - 1
         last_node_index = last_index - 1 if self.perspective_player == last_index else last_index
@@ -50,14 +71,13 @@ class SimultaneousOpenLoopNode:
             if i == self.perspective_player:
                 continue
             node = descend_tree(node, m, chance_node=(last_node_index != i))
-        # Then return final node after all players have acted
-        return node
+        return node  # Final node, after all players have acted
 
-    def add_child(self, move, state=None):
+    def add_child(self, move: int, state: gym.Env = None):
         """
         Adds a new child node to this Node.
-        :param move: (int) action taken by the player
-        :param state: (GameState) state corresponding to new child node
+        :param move: action taken by the player
+        :param state: state corresponding to new child node
         :returns: new expanded node added to the tree
         """
         node = SimultaneousOpenLoopNode(self.perspective_player, move=move, parent=self, state=state)
@@ -65,7 +85,7 @@ class SimultaneousOpenLoopNode:
         self.child_nodes.append(node)
         return node
 
-    def update(self, result):
+    def update(self, result: float):
         """
         Updates the node statistics saved in this node with the param result
          which is the information obtained during the latest rollout.
@@ -74,7 +94,12 @@ class SimultaneousOpenLoopNode:
         self.visits += 1
         self.wins += result
 
-    def __repr__(self, indent=0, ignore_chance=True):
+    def __repr__(self, indent=0, ignore_chance=True) -> str:
+        '''
+        For debugging purposes. Prints the tree starting with
+        :param: self as the root with corresponding statistics.
+        Source code is messy, it was made in a jam ;).
+        '''
         s = ''
         if self.parent_node is not None and self.parent_node.is_chance_node and ignore_chance:
             pass
--- regym/tests/rl_algorithms/mcts_test.py
+++ regym/tests/rl_algorithms/mcts_test.py
@@ -14,6 +14,7 @@ def test_non_integer_budget_raises_value_error(Connect4Task):
     with pytest.raises(ValueError) as _:
         _ = build_MCTS_Agent(Connect4Task, config, 'name')
 
+
 def test_mcts_can_take_actions_discrete_obvservation_discrete_action(Connect4Task, mcts_config_dict):
     mcts1 = build_MCTS_Agent(Connect4Task, mcts_config_dict, agent_name='MCTS1-test')
     mcts2 = build_MCTS_Agent(Connect4Task, mcts_config_dict, agent_name='MCTS1-test')
@@ -26,9 +27,9 @@ def test_can_defeat_random_play_in_connect4_both_positions(Connect4Task, mcts_co
     mcts2 = build_MCTS_Agent(Connect4Task, mcts_config_dict, agent_name='MCTS2-test')
     trajectory = Connect4Task.run_episode([mcts1, mcts2], training=False)
 
-    assert extract_winner(trajectory) == 1 # Second player (index 1) has a much higher budget
+    assert extract_winner(trajectory) == 1  # Second player (index 1) has a much higher budget
     trajectory = Connect4Task.run_episode([mcts2, mcts1], training=False)
-    assert extract_winner(trajectory) == 0 # First player (index 0) has a much higher budget
+    assert extract_winner(trajectory) == 0  # First player (index 0) has a much higher budget
 
 
 def test_can_coordinate_in_random_walk(RandomWalkTask, mcts_config_dict):
@@ -37,7 +38,7 @@ def test_can_coordinate_in_random_walk(RandomWalkTask, mcts_config_dict):
     mcts1 = build_MCTS_Agent(RandomWalkTask, mcts_config_dict, agent_name='MCTS1-test')
     mcts2 = build_MCTS_Agent(RandomWalkTask, mcts_config_dict, agent_name='MCTS2-test')
 
-    expected_end_state = [3, 3] # TODO, don't hardcode value
+    expected_end_state = [3, 3]  # TODO, don't hardcode value
 
     trajectory = RandomWalkTask.run_episode([mcts1, mcts2], training=False)
 
