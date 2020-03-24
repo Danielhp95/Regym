7bcebfd4badb0dfd1a1ada91fa967fd569716d90
daniel <danielhp95@gmail.com>
2020-03-24 16:05:47 +0000

    Refactored __init__ to make it more informative

--- regym/rl_algorithms/agents/__init__.py
+++ regym/rl_algorithms/agents/__init__.py
@@ -1,11 +1,20 @@
 from .agent import Agent
+
+# Useful basic agents
 from .random_agent import build_Random_Agent, RandomAgent
 from .mixed_strategy_agent import MixedStrategyAgent
+
+# Tabular agents
 from .tabular_q_learning_agent import build_TabularQ_Agent, TabularQLearningAgent
+
+# DQN based agents
 from .deep_q_network_agent import build_DQN_Agent, DeepQNetworkAgent
+
+# Policy gradient agents
 from .ppo_agent import build_PPO_Agent, PPOAgent
 from .reinforce_agent import build_Reinforce_Agent, ReinforceAgent
 from .a2c_agent import build_A2C_Agent, A2CAgent
+
 from .mcts_agent import build_MCTS_Agent, MCTSAgent
 
 rockAgent     = MixedStrategyAgent(support_vector=[1, 0, 0], name='RockAgent')
