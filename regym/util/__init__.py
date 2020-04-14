1a0a5b9d3887b28546fb24335a9775480727e40d
daniel <danielhp95@gmail.com>
2020-04-14 18:22:31 +0200

    Added profiling decorator to regym.util

--- regym/util/__init__.py
+++ regym/util/__init__.py
@@ -1,2 +1,4 @@
 from .play_matches import play_single_match, play_multiple_matches
 from .play_matches import extract_winner, extract_cumulative_rewards
+
+from .profiling import profile
---
+++ regym/util/profiling.py
@@ -0,0 +1,31 @@
+import cProfile
+import pstats
+import io
+
+
+def profile(filename: str = ''):
+    '''
+    A decorator that uses cProfile to profile a function.
+    If :param: filename is used, a pstats.Stats object will
+    be stored in a file under the same name. Otherwise the
+    profiling will be printed to standard output.
+    NOTE: A function which uses this profiling MUST NOT have
+          a keyword argument called 'filename'.
+
+    :param filename: string path to file where pstats.Stats will be saved
+    '''
+
+    def wrap(func):
+        def wrapped_f(*args, **kwargs):
+            import ipdb; ipdb.set_trace()
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = func(*args, **kwargs)
+            pr.disable()
+            sortby = 'cumulative'
+            ps = pstats.Stats(pr).sort_stats(sortby)
+            if filename != '': ps.dump_stats(filename)
+            else: print(ps.print_stats())
+            return retval
+        return wrapped_f
+    return wrap
