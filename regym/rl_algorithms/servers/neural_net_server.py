af80e5f736417a2d6616c5e719db05cee166b1d4
daniel <danielhp95@gmail.com>
2020-06-23 15:08:14 +0200

    Added NeuralNetServerHandler with capability of holding a server and updating its underlying neural network.

---
+++ regym/rl_algorithms/servers/neural_net_server.py
@@ -0,0 +1,104 @@
+from typing import Callable, Dict, List
+import multiprocessing
+from copy import deepcopy
+import torch
+
+from regym.rl_algorithms.networks.preprocessing import batch_vector_observation
+
+
+class NeuralNetServerHandler:
+
+    def __init__(self, num_connections: int,
+                 net: torch.nn.Module,
+                 pre_processing_fn: Callable = batch_vector_observation,
+                 device: str = 'cpu'):
+        '''
+        NOTE: net will be deepcopied
+
+        :param num_connections: TODO
+        :param net: TODO
+        :param pre_processing_fn: TODO
+        :param device: TODO
+        '''
+        self.num_connections = num_connections
+        self.device = device
+        self.pre_processing_fn = pre_processing_fn
+
+        self.server_connections, self.client_connections = [], []
+        for _ in range(num_connections):
+            server_connection, client_connection = multiprocessing.Pipe()
+            self.server_connections.append(server_connection)
+            self.client_connections.append(client_connection)
+
+        self.server = multiprocessing.Process(
+                target=neural_net_server,
+                args=(deepcopy(net), self.server_connections,
+                      self.pre_processing_fn, self.device))
+        self.server.start()
+
+
+    def update_neural_net(self, net):
+        # Delete server, create new server with a deepcopy of :param: net
+        self.server.terminate()
+        self.server = multiprocessing.Process(
+                target=neural_net_server,
+                args=(deepcopy(net), self.server_connections,
+                      self.pre_processing_fn, self.device))
+        self.server.start()
+
+    def close_server(self):
+        self.server.terminate()
+
+    # TODO: __repr__ function
+
+
+def neural_net_server(net: torch.nn.Module,
+                      connections: List[multiprocessing.Pipe],
+                      pre_processing_fn: Callable = batch_vector_observation,
+                      device: str = 'cpu'):
+    """
+    Server style function which continuously polls :params: parent_connections
+    for observations (inputs) to be fed to torch.nn.Module :param: net.
+    The neural net will be loaded onto :param: device (i.e, cpu, gpu:0)
+
+    ASSUMPTION: Requests want individual observations, never batches.
+
+    :param net: TODO
+    :param pre_processing_fn: TODO
+    :param connections: TODO
+    :param device: TODO
+    """
+    net.to(device)
+    # Maybe we can refactor to make this simpler
+    pipes_to_serve, batch = [], []
+    while True:
+        for conn in connections:
+            if conn.poll():
+                pipes_to_serve.append(conn)
+                batch.append(conn.recv())
+        if batch:
+            pre_processed_batch = pre_processing_fn(batch)
+            prediction = net(pre_processed_batch)
+
+            responses = _generate_responses(len(pipes_to_serve), prediction)
+
+            _send_responses(pipes_to_serve, responses)
+
+            pipes_to_serve.clear()
+            batch.clear()
+
+
+def _generate_responses(num_pipes_to_serve: int,
+                        prediction: Dict[str, torch.Tensor]) \
+                        -> List[Dict[str, torch.Tensor]]:
+    '''Generates a response for each pipe that sent a request to the server'''
+    responses = [{} for _ in range(num_pipes_to_serve)]
+    for k, v in prediction.items():
+        for i in range(num_pipes_to_serve):
+            responses[i][k] = v[i].cpu().detach()
+    return responses
+
+
+def _send_responses(pipes_to_serve: List[multiprocessing.Pipe],
+                    responses: List[Dict[str, torch.Tensor]]):
+    for i in range(len(pipes_to_serve)): pipes_to_serve[i].send(responses[i])
---
+++ regym/tests/rl_algorithms/neural_net_server_test.py
@@ -0,0 +1,89 @@
+from copy import deepcopy
+import multiprocessing
+import numpy as np
+import torch
+
+from regym.rl_algorithms.servers import neural_net_server
+from regym.rl_algorithms.servers.neural_net_server import NeuralNetServerHandler
+
+
+def test_can_process_single_observation():
+    client_connection1, server_connection1 = multiprocessing.Pipe()
+    net = generate_dummy_neural_net(weight=1.)
+
+    server = multiprocessing.Process(
+            target=neural_net_server,
+            args=(deepcopy(net), [server_connection1]))
+
+    observation_1 = np.array([0]) 
+    client_connection1.send(observation_1)
+
+    server.start()
+
+    expected_response_1 = {'output': torch.Tensor([0])}
+    assert expected_response_1 == client_connection1.recv()
+    server.terminate()
+    
+
+def test_can_process_batch_observation_and_respond_individually():
+    client_connection1, server_connection1 = multiprocessing.Pipe()
+    client_connection2, server_connection2 = multiprocessing.Pipe()
+
+    net = generate_dummy_neural_net(weight=1.)
+
+    server = multiprocessing.Process(
+            target=neural_net_server,
+            args=(deepcopy(net), [server_connection1, server_connection2]))
+
+    observation_1 = np.array([0]) 
+    observation_2 = np.array([1]) 
+
+
+    client_connection1.send(observation_1)
+    client_connection2.send(observation_2)
+
+    server.start()
+
+    expected_response_1 = {'output': torch.Tensor([0])}
+    expected_response_2 = {'output': torch.Tensor([1])}
+
+    assert expected_response_1 == client_connection1.recv()
+    assert expected_response_2 == client_connection2.recv()
+    server.terminate()
+
+
+def test_can_update_the_neural_net_in_the_server():
+    net1 = generate_dummy_neural_net(weight=0.)
+    net2 = generate_dummy_neural_net(weight=1.)
+
+    observation = np.array([1])
+
+    expected_response_1 = {'output': torch.Tensor([0])}
+    expected_response_2 = {'output': torch.Tensor([1])}
+
+    server_handler = NeuralNetServerHandler(num_connections=1,
+                                            net=net1)
+
+    server_handler.client_connections[0].send(observation)
+    actual_response = server_handler.client_connections[0].recv()
+    assert expected_response_1 == actual_response 
+
+    server_handler.update_neural_net(net2)
+
+    server_handler.client_connections[0].send(observation)
+    actual_response = server_handler.client_connections[0].recv()
+    assert expected_response_2 == actual_response 
+
+    server_handler.close_server()
+
+
+def generate_dummy_neural_net(weight):
+    class DummyNet(torch.nn.Module):
+        def __init__(self, weight):
+            super().__init__()
+            self.linear = torch.nn.Linear(in_features=1, out_features=1, bias=False)
+            self.linear.weight.data = torch.Tensor([[weight]])
+        def forward(self, x):
+            return {'output': self.linear(x)}
+
+    return DummyNet(weight)
