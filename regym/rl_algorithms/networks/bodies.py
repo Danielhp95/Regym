b59f79a5650104235c1e9c75cf688899aad8e0df
daniel <danielhp95@gmail.com>
2020-06-24 21:39:35 +0200

    Removed unnused body

--- regym/rl_algorithms/networks/bodies.py
+++ regym/rl_algorithms/networks/bodies.py
@@ -28,24 +28,6 @@ class SequentialBody(nn.Module):
         return self.sequence(x)
 
 
-class NatureConvBody(nn.Module):
-    def __init__(self, in_channels=4):
-        super(NatureConvBody, self).__init__()
-        self.feature_dim = 512
-        self.conv1 = layer_init(nn.Conv2d(in_channels, 32, kernel_size=8, stride=4))
-        self.conv2 = layer_init(nn.Conv2d(32, 64, kernel_size=4, stride=2))
-        self.conv3 = layer_init(nn.Conv2d(64, 64, kernel_size=3, stride=1))
-        self.fc4 = layer_init(nn.Linear(7 * 7 * 64, self.feature_dim))
-
-    def forward(self, x):
-        y = F.relu(self.conv1(x))
-        y = F.relu(self.conv2(y))
-        y = F.relu(self.conv3(y))
-        y = y.view(y.size(0), -1)
-        y = F.relu(self.fc4(y))
-        return y
-
-
 class Convolutional2DBody(nn.Module):
     def __init__(self, input_shape: Tuple[int, int],
                  channels: List[int], kernel_sizes: List[int],
