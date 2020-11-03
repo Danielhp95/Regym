01d5bc998287873a1093302ebdf1211fffddda13
daniel <danielhp95@gmail.com>
2020-11-03 18:50:45 +0100

    Added test to check (1) That apprentice can perform 1 update towards expert and (2) Opponent modelling can be performed

--- regym/tests/rl_algorithms/expert_iteration_test.py
+++ regym/tests/rl_algorithms/expert_iteration_test.py
@@ -1,13 +1,15 @@
+from typing import Callable
+
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
 from test_fixtures import expert_iteration_config_dict, mcts_config_dict, Connect4Task
 import regym
-
 from regym.tests.test_utils.play_against_fixed_opponent import learn_against_fix_opponent
 from regym.tests.test_utils.parallel_play_against_fixed_opponent import parallel_learn_against_fix_opponent
-from regym.rl_algorithms import rockAgent, build_Random_Agent, build_MCTS_Agent
+from regym.rl_algorithms import rockAgent, build_Random_Agent, build_MCTS_Agent, build_Deterministic_Agent
 from regym.rl_algorithms import build_ExpertIteration_Agent
+from regym.rl_algorithms.replay_buffers import Storage
 
 
 def test_expert_iteration_can_take_actions_discrete_obvservation_discrete_action(Connect4Task, expert_iteration_config_dict):
@@ -59,8 +61,72 @@ def test_can_collect_opponent_action_distributions_multi_env(Connect4Task, exper
         num_envs=2, num_episodes=2)
     # We only check for existance of the key, rather than it's content
     assert 'opponent_policy' in ex_it.algorithm.memory.keys
+    assert 'opponent_s' in ex_it.algorithm.memory.keys
     # ex_it.algorithm.memory. Once you fix it. push!
     assert len(ex_it.algorithm.memory.opponent_policy) == len(ex_it.algorithm.memory.s)
+    assert len(ex_it.algorithm.memory.opponent_policy) == len(ex_it.algorithm.memory.opponent_s)
+
+
+def test_apprentice_can_model_expert(Connect4Task, expert_iteration_config_dict):
+    expert_iteration_config_dict['use_agent_modelling'] = False
+
+    expert_iteration_config_dict['batch_size'] = 10
+    expert_iteration_config_dict['num_epochs_per_iteration'] = 10
+
+    _test_learn_against_fixed_distribution(
+        Connect4Task,
+        expert_iteration_config_dict,
+        prediction_process_fn=lambda p: p['probs']
+    )
+
+
+def test_can_model_fixed_stochastic_policy(Connect4Task, expert_iteration_config_dict):
+    expert_iteration_config_dict['use_agent_modelling'] = True
+
+    expert_iteration_config_dict['batch_size'] = 10
+    expert_iteration_config_dict['num_epochs_per_iteration'] = 10
+
+    _test_learn_against_fixed_distribution(
+        Connect4Task,
+        expert_iteration_config_dict,
+        prediction_process_fn=lambda p: p['policy_0']['probs']
+    )
+
+
+def _test_learn_against_fixed_distribution(task, config, prediction_process_fn: Callable):
+    # Random stochstic policy
+    target_policy = torch.Tensor([0.17, 0.03, 0.05, 0.4, 0.05, 0.2, 0.1])
+
+    # Create agent and dataset
+    memory = create_memory(100, target_policy, task)
+    ex_it = build_ExpertIteration_Agent(task, config, agent_name='ExIt-opponent_modelling-test')
+    ex_it.algorithm.memory = memory
+
+    # Train
+    num_trains = 4
+    for _ in range(num_trains):
+        ex_it.algorithm.train(ex_it.apprentice)
+
+    # Test
+    _test_model(ex_it, target_policy, task, prediction_process_fn)
+
+
+def _test_model(agent, target_policy, task, prediction_process_fn,
+                test_iterations=50, kl_divergence_tolerance=0.01):
+    for i in range(test_iterations):
+        with torch.no_grad():
+            sample_obs = agent.state_preprocess_fn(
+                task.env.observation_space.sample()[0]
+            )
+            processed_prediction = prediction_process_fn(agent.apprentice(sample_obs))
+            kl_div = torch.nn.functional.kl_div(
+                processed_prediction.log(),
+                target_policy
+            )
+            assert kl_div.item() < kl_divergence_tolerance, (f'Prection was too far off from allowed tolerance {kl_divergence_tolerance}\n'
+                                                             f'Target distribution: {target_policy}\n'
+                                                             f'Prediction: {processed_prediction}\n'
+                                                             f'Diff: {torch.abs(target_policy - processed_prediction)}')
 
 
 def test_can_use_apprentice_in_expert_in_expansion_and_rollout_phase(Connect4Task, expert_iteration_config_dict):
@@ -108,6 +174,21 @@ def test_train_apprentice_using_dagger_against_random_connect4(Connect4Task, exp
             evaluation_method='last',
             show_progress=True,
             summary_writer=summary_writer)
+
+
+def create_memory(size: int,
+                  fixed_policy_target: torch.Tensor,
+                  task) -> Storage:
+    memory = Storage(size, keys=['opponent_policy', 'opponent_s', 's', 'V', 'normalized_child_visitations'])
+    for _ in range(size):
+        random_opponent_s = torch.rand((1, *task.observation_dim))
+        random_s = torch.rand((1, *task.observation_dim))
+        memory.add({'s': random_s,
+                    'V': torch.Tensor([0.]),
+                    'normalized_child_visitations': fixed_policy_target,
+                    'opponent_s': random_opponent_s,
+                    'opponent_policy': fixed_policy_target})
+    return memory
 
 
 def test_train_vanilla_exit_against_random_connect4(Connect4Task, expert_iteration_config_dict, mcts_config_dict):
