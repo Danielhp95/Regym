caaf4343b73acddbc959f53c5821087a4b8654cd
daniel <danielhp95@gmail.com>
2021-01-11 21:17:02 +0100

    Added MCTS consistency test

--- regym/rl_algorithms/MCTS/selection_strategies.py
+++ regym/rl_algorithms/MCTS/selection_strategies.py
@@ -50,5 +50,5 @@ def PUCT(node: SequentialNode, c: float) -> Dict[int, float]:
               child node present in :param: node
     '''
     return {a_i: node.Q_a[a_i] +
-                 c * node.P_a[a_i] * (sqrt(node.N) / (node.N_a[a_i] + 1))
+            c * node.P_a[a_i] * (sqrt(node.N) / (node.N_a[a_i] + 1))
             for a_i in node.actions}
--- regym/tests/rl_algorithms/mcts_test.py
+++ regym/tests/rl_algorithms/mcts_test.py
@@ -1,3 +1,5 @@
+from functools import partial, reduce
+
 import pytest
 import numpy as np
 
@@ -6,7 +8,106 @@ from test_fixtures import mcts_config_dict, Connect4Task, RandomWalkTask
 
 from regym.rl_algorithms.agents import build_MCTS_Agent, build_Random_Agent, build_NeuralNet_Agent
 from regym.networks.servers import request_prediction_from_server
+from regym.rl_algorithms.MCTS import selection_strategies
+from regym.rl_algorithms.MCTS import sequential_mcts
+from regym.rl_algorithms.MCTS import util
 
+def test_sequential_mcts_consistency_on_tree_properties_in_connect4(Connect4Task):
+    '''
+    - Root node should have the same number of visitations as the given budget.
+    - A node's visit should be the sum of its children's visits
+    '''
+    # For all subnodes, parent.N_a[a_i] == sum(child.N, parent.children[a_i].children)
+
+    budget = 100
+    rollout_budget = 100
+
+    done = False
+    observations = Connect4Task.env.reset()
+    while not done:
+        #p1_observation = observations[current_player]
+
+        best_action, _, tree = sequential_mcts.MCTS(
+            rootstate=Connect4Task.env,
+            observation=None,
+            budget=budget,
+            rollout_budget=rollout_budget,
+            selection_strat=selection_strategies.PUCT,
+            exploration_factor=1.0,
+            # It doesn't matter that we always act as player 0
+            player_index=0,
+            policy_fn=partial(util.random_selection_policy,
+                              action_dim=Connect4Task.action_dim),
+            evaluation_fn=None,
+            use_dirichlet=False,
+            dirichlet_alpha=None,
+            num_agents=2
+        )
+        _assert_only_top_node_is_root(tree)
+        _assert_terminal_nodes_have_correct_values_in_deterministic_connect4(tree)
+        #_assert_visitations_add_up(tree, budget, ['R'])
+
+        _, _, done, _ = Connect4Task.env.step(best_action)
+
+
+def _assert_only_top_node_is_root(node):
+    ''' TODO: check and document '''
+    if node.parent is None:
+        assert node.is_root, ('Node with no parent (root node) should have '
+                              'root property set')
+    else:
+        assert not(node.parent is None), 'Non-root node should have a parent node'
+        assert not(node.is_root), 'Node should not be root'
+    for child_node in node.children.values():
+        _assert_only_top_node_is_root(child_node)
+
+
+def _assert_visitations_add_up(node, budget, action_sequence):
+    if node.is_root:
+        assert node.N == budget, (
+             'Root node should have the same number of '
+             f'visitations as budget. Expected: {budget}. Actual: {node.N}')
+
+    child_visitations = sum(node.N_a.values())
+    assert node.N == child_visitations, (
+           'The visits of a node should be equal '
+            'to the sum of the edge visits stored in that node.\n '
+             f'Sum N_a: {child_visitations}. N: {node.N}\n'
+             f'Branch path action sequence: {action_sequence}\n'
+             f'{node}')
+
+    for child_node in node.children.values():
+        _assert_visitations_add_up(child_node, budget, action_sequence + [child_node.a])
+
+
+def _assert_terminal_nodes_have_correct_values_in_deterministic_connect4(node):
+     # Terminal nodes have value == N or -N (it is a deterministic game). Check that there are no draws?
+     if node.is_terminal:
+         total_node_value = node.parent.W_a[node.a]
+         avg_node_value   = node.parent.Q_a[node.a]
+         print(total_node_value, avg_node_value, node.N)
+         assert total_node_value == avg_node_value * node.N, (
+             "In a deterministic environment, a terminal node's total and average "
+             "value should be proportional to the number of visits.\n"
+             f"Total {total_node_value}. Avg: {avg_node_value}. Visits {node.N}"
+             )
+         assert ((total_node_value == 0.) or \
+                 (total_node_value == -node.N) or \
+                 (total_node_value == node.N)), (
+                     "In a deterministic environment with -1 (loss) / 0 (draw) / +1 (win) "
+                     "final scores, a terminal node's value should be either zero or "
+                     "equal to its visit count.\n"
+                     f"Actual value: {total_node_value}. Visits {node.N}"
+                 )
+
+     for child_node in node.children.values():
+         _assert_terminal_nodes_have_correct_values_in_deterministic_connect4(child_node)
+
+
+#def test_PUCT_selection_strategies_correctly_uses_priors():
+#    # check that PUCT doesn't return 0.
+#    # use PUCT, already imported
+#    assert False
 
 
 def test_non_integer_budget_raises_value_error(Connect4Task):
